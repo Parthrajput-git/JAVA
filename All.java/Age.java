c88a9af91dee8112598211eded8169fecf858b4e
Parthrajput-git <pr9410350@gmail.com>
2025-10-10 17:35:24 +0530

    Update Age.java

--- All.java/Age.java
+++ All.java/Age.java
@@ -3,7 +3,7 @@ public class Age{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-        //int b=sc.nextInt()
+        //int b=sc.nextInt();
         if(a>=18){
             System.out.println("The available for voting  :");
         }
