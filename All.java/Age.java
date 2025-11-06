75267e3dae6aefa55ff393da48cca592778e36a3
@Parth Rajput <pr9410350@gmail.com>
2025-11-06 21:42:50 +0530

    Comment out main method in Age.java
    Comment out main method and scanner initialization

--- All.java/Age.java
+++ All.java/Age.java
@@ -1,8 +1,8 @@
 // import java.util.Scanner;
 // public class Age{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
+    // public static void main(String args[]){
+    //     Scanner sc=new Scanner(System.in);
+    //     int a=sc.nextInt();
         if(a>=18){
             System.out.println("The available for voting  :");
         }
