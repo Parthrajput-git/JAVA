cefbb9d5fe0c5f4c31e7f5680491ebedc4defc37
@Parth Rajput <pr9410350@gmail.com>
2025-11-06 21:44:28 +0530

    Update Age.java

--- All.java/Age.java
+++ All.java/Age.java
@@ -1,7 +1,7 @@
 // import java.util.Scanner;
 // public class Age{
-    // public static void main(String args[]){
-    //     Scanner sc=new Scanner(System.in);
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         if(a>=18){
             System.out.println("The available for voting  :");
