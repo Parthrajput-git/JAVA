65313190a34b80ee244ac167b5c1de960adaa76f
@Parth Rajput <pr9410350@gmail.com>
2025-11-11 23:15:16 +0530

    Update Max.java

--- ArrayLists/Max.java
+++ ArrayLists/Max.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 public class Max{
     public static void main(String args[]){
-        // ArrayList<Integer> list=new ArrayList<>();
+        ArrayList<Integer> list=new ArrayList<>();
         list.add(2);
         list.add(5);
         list.add(9);
