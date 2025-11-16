40a6ac398ea24d0d205961f10cf11acb73f17cc0
@Parth Rajput <pr9410350@gmail.com>
2025-11-16 22:36:59 +0530

    Update Fact.java

--- All.java/Fact.java
+++ All.java/Fact.java
@@ -3,7 +3,7 @@
 public class Fact{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        int f=1;
+        int fact=1;
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             f*=i;
