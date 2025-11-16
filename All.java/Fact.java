e614b6b5e9d09e856a1a719f1e4196a71c49ee6c
@Parth Rajput <pr9410350@gmail.com>
2025-11-16 22:37:23 +0530

    Update Fact.java

--- All.java/Fact.java
+++ All.java/Fact.java
@@ -1,6 +1,6 @@
 // import java.util.Scanner;
 
-// public class Fact{
+public class Fact{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int fact=1;
