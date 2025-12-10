7668bcd3dbce9011c0544eef8ea5ac62d982364d
@Parth Rajput <pr9410350@gmail.com>
2025-12-10 19:06:02 +0530

    Update Age.java

--- All.java/Age.java
+++ All.java/Age.java
@@ -1,5 +1,5 @@
 // import java.util.Scanner;
-public class Age{
+// public class Age{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
